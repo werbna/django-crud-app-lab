d7da0d8a620b253240df94d2bc9f37476e55d558
Andrew <andrewchau1993@gmail.com>
2024-09-11 14:22:28 -0700

    loaded in template recipes to text index page

--- my_app/urls.py
+++ my_app/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path # type: ignore
+from django.urls import path  # type: ignore
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('recipe/', views.recipes_index, name='recipes_index'),
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render # type: ignore
-from django.http import HttpResponse # type: ignore
+
+class Recipe:
+    def __init__(self, day, name, description):
+        self.day = day
+        self.name = name
+        self.description = description
+        
+recipes = [
+    Recipe('Monday', 'Chicken Alfredo', 'A delicious pasta dish with chicken and alfredo sauce.'),
+    Recipe('Tuesday', 'Spaghetti and Meatballs', 'A classic Italian dish with spaghetti noodles and meatballs.'),
+    Recipe('Wednesday', 'Tacos', 'A Mexican dish with seasoned beef and tortillas.'),
+    Recipe('Thursday', 'Chicken Parmesan', 'An Italian dish with breaded chicken and marinara sauce.'),
+    Recipe('Friday', 'Fish and Chips', 'A British dish with fried fish and french fries.')
+]   
 
 # Create your views here.
 def home(req):
-    return HttpResponse('Hello, World!')
-  
+    return render(req, 'home.html')
 def about(req):
     return render(req, 'about.html')
+def recipe(req):
+    return render(req, 'recipes/recipe.html', {'recipe': recipes})
